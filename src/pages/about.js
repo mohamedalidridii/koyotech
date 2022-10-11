11ba50a1d7d08666b4904e8bc4905141fa758d18
mohamedalidridii <mohamedali.dridi1@esprit.tn>
2022-10-11 18:16:57 +0200

    About page modified

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,17 +5,23 @@ const About = () => {
     <div className='page'>
       <div className='container'>
         
-          <h1 className="title-about">ABOUT</h1>
+          
           <div className='About-subtitle'>
+          <h1>ABOUT</h1>
           <p>Driven by teamwork, digital solutions & the people that use them. We are a Tunis-based digital creative agency that focuses on the strategy, design and development of digital products for great people.</p>
+          <img src="https://images.unsplash.com/photo-1517048676732-d65bc937f952?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt=""/>
           <div>
-            <p>our</p>
-            <h1 className="title-ourVision">VISION</h1>
+            
+            <h1 className="title-ourVision"><p>our</p>VISION</h1>
             <h2 className="sub-subtitle-right">1. FROM START TO FINISH</h2>
             <p className="text-sub-subtitle-right">We provide value to our clients through design thinking and customized tech stacks. Our designers, full-stack developers,project managers and strategists are working in closely-knit teams throughout every project.</p>
+            
             <h2 className="sub-subtitle-left">2. DRIVEN BY INSIGHTS</h2>
             <p className="text-sub-subtitle-left">We truely believe that good research leads to effective design, robust tech stacks and trialor-made outcomes. Powerful concepts are driven by stirring insights.</p>
-
+            <h2 className="sub-subtitle-right">3. POWERED BY CREATIVITY</h2>
+            <p className="text-sub-subtitle-right">At KOYOTEC, creativity is what drives the agency. It manifests itself through our managamenet approach, creation process and technological development. This is why each web project is unique, just like you.</p>
+            <h2 className="sub-subtitle-left">4. OBSESSED WITH RESULTS</h2>
+            <p className="text-sub-subtitle-left">We DEVELOP DIGITAL SOLUTIONS? PLATFORMS AND MODULES THAT HAVE IMPACT FOR OUR CLIENTS. We firmly believe that crating tailor-made digital projects is essential to the success of tomorrow's thriving businesses.</p>
           </div>
         </div>
       </div>
